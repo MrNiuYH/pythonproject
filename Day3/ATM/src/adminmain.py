7a12a7e0b2a8519ea4c040758a0954b015628cba
dell <dell@gmail.com>
2016-11-08 18:40:04 +0800

    2016/11/8

---
+++ Day3/ATM/src/adminmain.py
@@ -0,0 +1,11 @@
+# /usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:Mr.Niu
+
+
+def add_user():    # 添加用户
+    pass
+
+
+def freeze_user():  # 冻结用户
+    pass
---
+++ Day3/ATM/src/usermain.py
@@ -0,0 +1,19 @@
+# /usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:Mr.Niu
+
+
+def deposit():    # 存款
+    pass
+
+
+def freeze_user():  # 取款
+    pass
+
+
+def transfer():  # 转账
+    pass
+
+
+def balance():  # 查看余额
+    pass
