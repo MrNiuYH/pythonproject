951fcc104248e1234a4f9230d6b90a817fdba951
dell <dell@gmail.com>
2016-12-12 20:33:41 +0800

    2016/11/18

---
+++ Day3/内置函数.py
@@ -0,0 +1,29 @@
+# /usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:Mr.Niu
+
+
+'''
+abs
+all
+any
+ascii
+bin
+bool
+bytes
+chr
+ord
+compile
+exec
+eval
+dict
+dir
+divmod
+enumerate
+filter
+map
+import functools    functools.reduce
+
+'''
+
+print("ass12".capitalize())
